cf54896f7bb04c99e1fa8261c592db34590e6a3a
Antony <86053464+AntonyLoose@users.noreply.github.com>
2022-04-06 18:03:41 +1000

    Commented potential solution for complex gestures

--- Gesture_handling/GestureDetector/app/src/main/java/com/talkbacktutorial/gesturedetector/MainActivity.kt
+++ Gesture_handling/GestureDetector/app/src/main/java/com/talkbacktutorial/gesturedetector/MainActivity.kt
@@ -28,8 +28,27 @@ class MainActivity : AppCompatActivity() {
                 A change has occurred during the touch event (between ACTION_DOWN and ACTION_UP).
                 An ACTION_MOVE contains the pointer's most recent x and y coordinates along with
                 any intermediate points since the last DOWN or MOVE event.
+
+                Could use this to check for advanced gestures?
+                Idea1: We have the x and y coordinates of each touch, therefore, on the first touch
+                we can construct a box (store coordinates of each corner) around the first touch,
+                along as the following touches are within the box the tracking continues (with) each
+                touch we move the box (to prevent going backwards). When the touch changes direction
+                we create a new box and repeat the previous steps.
+                The issue is we couldn't use this for sandbox mode as we need to know what gesture
+                they are attempting so we can construct the correct boxes.
+                We can test talkback gestures to estimate what sizes the boxes should be.
+                Diagram:
+                    ___________
+                    |     _____|___________________
+                    |     |    |  . ..       ...  |
+                    |     T ...|....  .......   T |
+                    |    .|____|__________________|
+                    |     .    |
+                    |     .    |
+                    |   .      |
+                    |_____T____|
                  */
-                // Could use this to create our own custom gestures?
                 setGestureText("x: ${event?.rawX.toString()} \n y: ${event?.rawY.toString()}")
                 true
             }
